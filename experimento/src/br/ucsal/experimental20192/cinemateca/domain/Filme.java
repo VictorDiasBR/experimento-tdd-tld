31dffb619ea5eb91214a502c1a5e9ff2d90dadee
Victor Dias <victor.assis@ucsal.edu.br>
2019-11-12 17:00:32 -0200

    almost done!

--- experimento/src/br/ucsal/experimental20192/cinemateca/domain/Filme.java
+++ experimento/src/br/ucsal/experimental20192/cinemateca/domain/Filme.java
@@ -1,5 +1,140 @@
 package br.ucsal.experimental20192.cinemateca.domain;
 
+import br.ucsal.experimental20192.cinemateca.enums.GeneroAtorEnum;
+
+import br.ucsal.experimental20192.cinemateca.enums.GeneroFilmeEnum;
+
 public class Filme {
- // TODO: Desenvolver a Classe Filme
+
+	private String titulo;
+    private int codigo;
+    private int anoLancamento;
+    private String sinopse;
+    private int qtdAtores = 20;
+    private Ator[] elenco = new Ator[qtdAtores];
+    //private int qtdFilmes = 20;
+    // private Filme[] filmes = new Filme[qtdFilmes ];
+    
+   
+	public Filme(String titulo, int anoLancamento, String sinopse) {
+        this.titulo = titulo;
+        this.anoLancamento = anoLancamento;
+        this.sinopse = sinopse;
+    }
+    
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getAnoLancamento() {
+        return anoLancamento;
+    }
+
+    public void setAnoLancamento(int anoLancamento) {
+        this.anoLancamento = anoLancamento;
+    }
+
+    public String getSinopse() {
+        return sinopse;
+    }
+
+    public void setSinopse(String sinopse) {
+        this.sinopse = sinopse;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public Ator[] getElenco() {
+        return elenco;
+    }
+    
+    //public Filme[] getFilmes() {
+    //	return filmes;
+    //}
+
+    //public void setFilme(Filme filme) {
+		
+    //for(int i = 0; i < qtdFilmes; i++){
+    //  if(filmes[i] == null){
+    //     filmes[i] = filme;
+    //       }
+    // }
+		
+    //}
+
+    public boolean incluirAtorElenco(Ator ator){
+        
+    	for(Ator a: elenco) {
+        if(a != null && ator.getNome().equals(a.getNome())) {
+            return false;
+        }
+        if(ator.getAnoNascimento() > anoLancamento) {
+        	return false;
+        }
+            }
+    	
+        for(int i = 0; i < qtdAtores; i++){
+            if(elenco[i] == null){
+                elenco[i] = ator;
+                return true;
+            }
+        }
+            return false;
+    }
+
+    public boolean removerAtorElenco(String nome){
+    	
+        for(int i = 0; i < qtdAtores; i++){
+        	
+            if(elenco[i] != null && elenco[i].getNome().equals(nome)){
+                elenco[i] = null;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Ator[] consultarElencoGenero(GeneroAtorEnum genero){
+    	
+        Ator[] elencoGenero = new Ator[20];
+        int cont = 0;
+        
+            for (int i  = 0; i < qtdAtores; i++){
+                if(elenco[i] != null && elenco[i].getGenero().equals(genero)){
+                    elencoGenero[cont] = elenco[i];
+                    cont++;
+                }
+            }
+            return elencoGenero;
+    }
+
+    public Ator pesquisarAtor(String nome){
+    	
+        for(Ator a: elenco) {
+            if(a != null && a.getNome().equals(nome)) {
+            	 return a;
+            }
+        } 
+        return null;
+    }
+
+    //public Ator[] consultarFilmePorGenero(GeneroFilmeEnum genero){
+    	
+    // Ator[] filmeGenero = new Ator[20];
+    //  int cont = 0;
+        
+    //  for (int i  = 0; i < qtdFilmes; i++){
+    //    if(filmes[i] != null && filmes[i].getGenero().equals(genero)){
+    //         filmeGenero[cont] = filmes[i];
+    //          cont++;
+    //        }
+    //    }
+    //      return filmeGenero;
+    // }
 }
--- experimento/test/br/ucsal/experimental20192/cinemateca/domain/ExemploTest.java
+++ experimento/test/br/ucsal/experimental20192/cinemateca/domain/ExemploTest.java
@@ -1,7 +1,10 @@
 package br.ucsal.experimental20192.cinemateca.domain;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import br.ucsal.experimental20192.cinemateca.enums.GeneroAtorEnum;
 
 public class ExemploTest {
 
@@ -20,6 +23,82 @@ public class ExemploTest {
 		// Execução do método a ser testado
 
 		// Comparação do valor retornado com o esperado
+
+	}
+	
+	@Test 
+	public void incluirAtorElenco(){
+	
+		Ator ator = new Ator("CJ", 1998, GeneroAtorEnum.MASCULINO);
+		
+		Filme filme = new Filme("GTA", 2006, "san andreas city bad stuffs");
+	
+		Assert.assertTrue(filme.incluirAtorElenco(ator));
+	}
+
+	@Test
+	public void incluirAtorMaisNovoFilme(){
+		
+		Ator ator = new Ator("CJ", 2008, GeneroAtorEnum.MASCULINO);
+		
+		Filme filme = new Filme("GTA", 2006, "san andreas city bad stuffs");
+		
+		Assert.assertFalse(filme.incluirAtorElenco(ator));
+	}
+
+	@Test 
+	public void incluirAtorRepetido(){
+		
+        Ator ator = new Ator("CJ", 1998, GeneroAtorEnum.MASCULINO);
+        Ator ator1 = new Ator("CJ", 1998, GeneroAtorEnum.MASCULINO);
+        
+		Filme filme = new Filme("GTA", 2006, "san andreas city bad stuffs");
+		filme.incluirAtorElenco(ator1);
+		
+		Assert.assertFalse(filme.incluirAtorElenco(ator));
+	}
+	
+	@Test 
+	public void removerAtorElenco(){
+		
+		Ator ator = new Ator("CJ", 1998, GeneroAtorEnum.MASCULINO);
+
+		Filme filme = new Filme("GTA", 2006, "san andreas city bad stuffs");
+		filme.incluirAtorElenco(ator);
+		
+		Assert.assertTrue(filme.removerAtorElenco("CJ"));
+	}
+
+	//
+	@SuppressWarnings("deprecation")
+	@Test
+	public void consultarElencoGeneroMasc(){
+		
+		Ator ator = new Ator("CJ", 1998, GeneroAtorEnum.MASCULINO);
+		Ator ator2 = new Ator("CJx", 1998, GeneroAtorEnum.MASCULINO);
+		Ator ator3 = new Ator("CJxx", 1998, GeneroAtorEnum.FEMININO);
+		
+		Filme filme = new Filme("GTA", 2006, "san andreas city bad stuffs");
+		
+		filme.incluirAtorElenco(ator);
+		filme.incluirAtorElenco(ator2);
+		filme.incluirAtorElenco(ator3);
+		
+		Ator[] esperado = {ator, ator2, null, null, null, null, null,
+				null, null, null, null, null, null, null, null, null, null, null, null, null};
+		
+		Assert.assertEquals(esperado, filme.consultarElencoGenero(GeneroAtorEnum.MASCULINO));
+	}
+	
+	@Test
+	public void pesquisarAtor(){
+		
+		Ator ator = new Ator("CJ", 1998, GeneroAtorEnum.MASCULINO);
+		Filme filme = new Filme("GTA", 2006, "san andreas city bad stuffs");
+
+		filme.incluirAtorElenco(ator);
+		
+		Assert.assertEquals(ator, filme.pesquisarAtor("CJ"));
 
 	}
 }
